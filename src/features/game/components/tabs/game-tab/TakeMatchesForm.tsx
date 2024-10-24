41023231a5304b4ac47dbedf0db362b2d70ddb5a
HoolyDuck <nazar260804@gmail.com>
2024-10-24 17:33:56 +0300

    * lint errors

--- src/features/game/components/tabs/game-tab/TakeMatchesForm.tsx
+++ src/features/game/components/tabs/game-tab/TakeMatchesForm.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
 
 type TakeMatchesFormProps = {
   handleMatchTake: (input: string) => void;
